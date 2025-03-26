6c8b2027116882e912a01df4fba99d5e324f315c
Manoj Neupane <maanojneupane111@gmail.com>
2025-03-26 20:15:51 +0545

    Update AuthProvider.jsx

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 
-export const AuthContext = createContext();
+export const AuthContext  = createContext();
 export default function AuthProvider({ children }) {
   const initialAuthUser = localStorage.getItem("Users");
   const [authUser, setAuthUser] = useState(
