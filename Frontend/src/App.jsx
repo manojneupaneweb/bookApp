edaf83cfa3c2e64ec4ed826abb0244415054155d
Manoj Neupane <maanojneupane111@gmail.com>
2025-06-05 09:46:13 +0545

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
-
+ 
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
